7d0292bfc1dd94f1a851766510880faa45acd678
alexgev <alexgev1997@gmail.com>
2018-02-10 01:10:12 +0300

    add webpack

--- server/app.js
+++ server/app.js
@@ -2,10 +2,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import {serverPort} from '../etc/config.json';
 import * as db from './utils/databaseUtils';
+import { dirname } from 'path';
+import path from "path";
 
 
 const app = express();
 app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../index.html'));
+// })
+
+// app.post('/', (req, res) => {
+//     console.log(req.body);
+//     res.send(req.body);
+// })
 
 app.get('/tasks', (req, res) => {
     db.findAll().then(
