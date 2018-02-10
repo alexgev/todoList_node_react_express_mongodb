9d0302c610a70d773635f70cf75b4dd51335e688
alexgev <alexgev1997@gmail.com>
2018-02-11 02:29:03 +0300

    react training

--- client/main.js
+++ client/main.js
@@ -2,35 +2,163 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-class Toggle extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {isToggledOn: true}
 
-        this.handleClick = this.handleClick.bind(this);
-    }
+function Warning(props) {
+	if (!props.warn) {
+		return null;
+	}
 
-    handleClick() {
-        this.setState((prevState) => {
-            return {isToggledOn: !prevState.isToggledOn}
-        })
-    }
+	return (
+		<div>
+			Warning Message!
+        </div>
+	)
+}
 
-    render() {
-        return (
-            <div>
-                <button onClick={this.handleClick}>
-                    {this.state.isToggledOn ? 'ON' : 'OFF'}
-                </button>
-            </div>
-        )
-    }
+class Page extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { warn: false };
+		this.handleToggleClick = this.handleToggleClick.bind(this);
+	}
+
+	handleToggleClick() {
+		this.setState(prevState => {
+			return { warn: !prevState.warn };
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<Warning warn={this.state.warn} />
+				<button onClick={this.handleToggleClick}>
+					{this.state.warn ? 'Hide' : 'Show'}
+				</button>
+			</div>
+		)
+	}
 }
 
 ReactDOM.render(
-    <Toggle />,
-    document.getElementById('app')
+	<Page />,
+	document.getElementById("app")
 )
+
+
+
+
+
+
+
+// function UserGreeting(props) {
+//     return <h1>Welcome Back</h1>
+// }
+
+// function GuestGreeting(props) {
+//     return <h1>Please, sign in</h1>
+// }
+
+// function Greeting(props) {
+//     const isLoggedIn = props.isLoggedIn;
+
+//     if(props.isLoggedIn) {
+//         return <UserGreeting />
+//     } else {
+//         return <GuestGreeting />
+//     }
+// }
+
+// function LogginButton(props) {
+//     return (
+//         <button onClick={props.onClick}>
+//             loggin
+//         </button>
+//     )
+// }
+
+// function LogoutButton(props) {
+//     return (
+//         <button onClick={props.onClick}>
+//             Logout
+//         </button>
+//     )
+// }
+
+// class LoginControl extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {isLoggedIn: false};
+//         this.handleLoginClick = this.handleLoginClick.bind(this);
+//         this.handleLogoutClick = this.handleLogoutClick.bind(this);
+//     }
+
+//     handleLoginClick() {
+//         this.setState({isLoggedIn: true});
+//     }
+
+//     handleLogoutClick() {
+//         this.setState({isLoggedIn: false});
+//     }
+
+//     render() {
+//         const isLoggedIn = this.state.isLoggedIn;
+
+//         let button = null;
+//         if (isLoggedIn) {
+//             button = <LogoutButton onClick={this.handleLogoutClick}/>
+//         } else {
+//             button = <LogginButton onClick={this.handleLoginClick} />
+//         }
+
+//         return (
+//             <div>
+//                 <Greeting isLoggedIn={isLoggedIn}/>
+//                 {button}
+//             </div>
+//         )
+//     }
+// }
+
+// ReactDOM.render(
+//     <LoginControl />,
+//     document.getElementById('app')
+// );
+
+
+
+
+
+
+// class Toggle extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {isToggledOn: true}
+
+//         this.handleClick = this.handleClick.bind(this);
+//     }
+
+//     handleClick() {
+//         this.setState((prevState) => {
+//             return {isToggledOn: !prevState.isToggledOn}
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <button onClick={this.handleClick}>
+//                     {this.state.isToggledOn ? 'ON' : 'OFF'}
+//                 </button>
+//             </div>
+//         )
+//     }
+// }
+
+// ReactDOM.render(
+//     <Toggle />,
+//     document.getElementById('app')
+// )
 
 
 
