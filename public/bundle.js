63f4d22ddf4881f255d71ebd33d4e9dc4c1ea719
alexgev <alexgev1997@gmail.com>
2018-02-11 21:14:06 +0300

    react training

--- public/bundle.js
+++ public/bundle.js
@@ -940,6 +940,8 @@ module.exports = focusNode;
 "use strict";
 
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(4);
 
 var _react2 = _interopRequireDefault(_react);
@@ -950,11 +952,523 @@ var _reactDom2 = _interopRequireDefault(_reactDom);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_reactDom2.default.render(_react2.default.createElement(
-    'div',
-    null,
-    'Hello react'
-), document.getElementById('app'));
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var scaleNames = {
+	c: "Celsius",
+	f: "Fahrenheit"
+};
+
+function toCelsius(fahrenheit) {
+	return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+	return celsius * 9 / 5 + 32;
+}
+
+function tryConvert(value, convert) {
+	var input = parseFloat(value);
+	if (Number.isNaN(input)) {
+		return '';
+	}
+	var output = convert(input);
+	var rounded = Math.round(output * 1000) / 1000;
+	return rounded.toString();
+}
+
+function BoilingVerdict(props) {
+	return _react2.default.createElement(
+		'p',
+		null,
+		+props.celsius >= 100 ? "yes" : "no"
+	);
+}
+
+var TemperatureInput = function (_React$Component) {
+	_inherits(TemperatureInput, _React$Component);
+
+	function TemperatureInput(props) {
+		_classCallCheck(this, TemperatureInput);
+
+		var _this = _possibleConstructorReturn(this, (TemperatureInput.__proto__ || Object.getPrototypeOf(TemperatureInput)).call(this, props));
+
+		_this.handleChange = _this.handleChange.bind(_this);
+		return _this;
+	}
+
+	_createClass(TemperatureInput, [{
+		key: 'handleChange',
+		value: function handleChange(e) {
+			this.props.onChange(e.target.value);
+		}
+	}, {
+		key: 'render',
+		value: function render() {
+			var value = this.props.value;
+			var scale = this.props.scale;
+			return _react2.default.createElement(
+				'fieldset',
+				null,
+				_react2.default.createElement(
+					'legend',
+					null,
+					'Enter temperature in ',
+					scaleNames[scale],
+					':'
+				),
+				_react2.default.createElement('input', { value: value,
+					onChange: this.handleChange })
+			);
+		}
+	}]);
+
+	return TemperatureInput;
+}(_react2.default.Component);
+
+var Calculator = function (_React$Component2) {
+	_inherits(Calculator, _React$Component2);
+
+	function Calculator(props) {
+		_classCallCheck(this, Calculator);
+
+		var _this2 = _possibleConstructorReturn(this, (Calculator.__proto__ || Object.getPrototypeOf(Calculator)).call(this, props));
+
+		_this2.handleCelsiusChange = _this2.handleCelsiusChange.bind(_this2);
+		_this2.handleFahrenheitChange = _this2.handleFahrenheitChange.bind(_this2);
+		_this2.state = { value: '', scale: 'c' };
+		return _this2;
+	}
+
+	_createClass(Calculator, [{
+		key: 'handleCelsiusChange',
+		value: function handleCelsiusChange(value) {
+			this.setState({ scale: 'c', value: value });
+		}
+	}, {
+		key: 'handleFahrenheitChange',
+		value: function handleFahrenheitChange(value) {
+			this.setState({ scale: 'f', value: value });
+		}
+	}, {
+		key: 'render',
+		value: function render() {
+			var scale = this.state.scale;
+			var value = this.state.value;
+			var celsius = scale === 'f' ? tryConvert(value, toCelsius) : value;
+			var fahrenheit = scale === 'c' ? tryConvert(value, toFahrenheit) : value;
+
+			return _react2.default.createElement(
+				'div',
+				null,
+				_react2.default.createElement(TemperatureInput, {
+					scale: 'c',
+					value: celsius,
+					onChange: this.handleCelsiusChange }),
+				_react2.default.createElement(TemperatureInput, {
+					scale: 'f',
+					value: fahrenheit,
+					onChange: this.handleFahrenheitChange }),
+				_react2.default.createElement(BoilingVerdict, {
+					celsius: celsius })
+			);
+		}
+	}]);
+
+	return Calculator;
+}(_react2.default.Component);
+
+_reactDom2.default.render(_react2.default.createElement(Calculator, null), document.getElementById('app'));
+
+// function BoilingVerdict(props) {
+// 	return (
+// 		<p>
+// 			{
+// 				+props.celsius >= 100 ? "yes" : "no"
+// 			}
+// 		</p>
+// 	)
+// } 
+
+// class Calculator extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {value: ''};
+// 		this.handleOnChange = this.handleOnChange.bind(this);
+// 	}
+
+// 	handleOnChange(e) {
+// 		this.setState({value: e.target.value})
+// 	}
+
+// 	render() {
+// 		const value = this.state.value;
+// 		return (
+// 			<fieldset>
+// 				<legend>Enter tempreture in Celsius:</legend>
+// 				<input value={this.state.value} onChange={this.handleOnChange}/>
+// 				<BoilingVerdict celsius={value}/>
+// 			</fieldset>
+// 		)
+// 	}
+// }
+
+// ReactDOM.render(
+// 	<Calculator />,
+// 	document.getElementById('app')
+// )
+
+
+// class Form extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {value: ''}
+// 		this.handleOnChange = this.handleOnChange.bind(this);
+// 		this.handleOnClick = this.handleOnClick.bind(this);
+// 	}
+
+// 	handleOnChange(event) {
+// 		this.setState({
+// 			value: event.target.value
+// 		})
+// 	}
+
+// 	handleOnClick(event) {
+// 		alert(`Text field value is ${this.state.value}`);
+// 	}
+
+
+// 	render() {
+// 		return (
+// 			<form>
+// 				<input placeholder="Hello!" value={this.state.value} onChange={this.handleOnChange} />
+// 				<button onClick={this.handleOnClick}>Submit</button>
+// 			</form>
+// 		)
+// 	}
+// }
+
+// ReactDOM.render(
+// 	<Form />,
+// 	document.getElementById('app')
+// )
+
+
+// ReactDOM.render(
+// 	<input type="text" value="hello!"/>,
+// 	document.getElementById('app')
+// )
+
+
+// function NumberList(props) {
+// 	const numbers = props.numbers;
+// 	return (
+// 		<ul>
+// 			{
+// 				numbers.map((val) => {
+// 					return <li key={val}>{val}</li>
+// 				})
+// 			}
+// 		</ul>
+// 	)
+// }
+
+// const numbers = [1,2,3,4,5];
+
+// ReactDOM.render(
+// 	<NumberList numbers={numbers}/>,
+// 	document.getElementById('app')
+// )
+
+
+// function Blog(props) {
+// 	const sidebar = props.posts.map((post) => {
+// 		return <li key={post.id}>{post.title}</li>
+// 	})
+
+// 	const content = props.posts.map((post) => {
+// 		return (
+// 			<div key={post.id}>
+// 				<h3>{post.title}</h3>
+// 				<p>{post.content}</p>
+// 			</div>
+// 		)
+// 	})
+
+// 	return (
+// 		<div>
+// 			<ul>
+// 				{sidebar}
+// 			</ul>
+// 			<hr />
+// 			<ul>
+// 				{content}
+// 			</ul>
+// 		</div>
+// 	)
+// }
+
+
+// const posts = [
+// 	{id: 1, title: "Hello world", content: "Start learning React js"},
+// 	{id: 2, title: "Installation", content: "You can install react from npm"}
+// ]
+
+// ReactDOM.render(
+// 	<Blog posts={posts} />,
+// 	document.getElementById('app')
+// )
+
+
+// function ListItem(props) {
+// 	const liElem = props.number;
+// 	return (<li>{props.number}</li>)
+// }
+
+// function NumberList(props) {
+// 	const arr = props.numbers;
+// 	const li = arr.map((val) => {
+// 		return <ListItem number={val} key={val}/>
+// 	})
+// 	return (
+// 		<ul>
+// 			{li}
+// 		</ul>
+// 	)
+// }
+
+// const arr = [1,2,3,4,5];
+
+// ReactDOM.render(
+// 	<NumberList numbers={arr} />,
+// 	document.getElementById('app')
+// )
+
+
+// function ListItems(props) {
+// 	const arr = props.number;
+// 	const li = arr.map((val) => {
+// 		return <li key={val.toString()}>{val}</li>
+// 	})
+// 	return <ul>{li}</ul>;
+// }
+
+// const numbers = [1,2,3,4,5];
+
+// ReactDOM.render(
+// 	<ListItems number={numbers}/>,
+// 	document.getElementById('app')
+// )
+
+
+// const arr = [1,2,3,4,5];
+// const numbersLi = arr.map((number) => {
+// 	return <li>{number}</li>
+// })
+
+// ReactDOM.render(
+// 	<ul>{numbersLi}</ul>,
+// 	document.getElementById('app')
+// )
+
+
+// function Warning(props) {
+// 	if (!props.warn) {
+// 		return null;
+// 	}
+
+// 	return (
+// 		<div>
+// 			Warning Message!
+//         </div>
+// 	)
+// }
+
+// class Page extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = { warn: false };
+// 		this.handleToggleClick = this.handleToggleClick.bind(this);
+// 	}
+
+// 	handleToggleClick() {
+// 		this.setState(prevState => {
+// 			return { warn: !prevState.warn };
+// 		});
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div>
+// 				<Warning warn={this.state.warn} />
+// 				<button onClick={this.handleToggleClick}>
+// 					{this.state.warn ? 'Hide' : 'Show'}
+// 				</button>
+// 			</div>
+// 		)
+// 	}
+// }
+
+// ReactDOM.render(
+// 	<Page />,
+// 	document.getElementById("app")
+// )
+
+
+// function UserGreeting(props) {
+//     return <h1>Welcome Back</h1>
+// }
+
+// function GuestGreeting(props) {
+//     return <h1>Please, sign in</h1>
+// }
+
+// function Greeting(props) {
+//     const isLoggedIn = props.isLoggedIn;
+
+//     if(props.isLoggedIn) {
+//         return <UserGreeting />
+//     } else {
+//         return <GuestGreeting />
+//     }
+// }
+
+// function LogginButton(props) {
+//     return (
+//         <button onClick={props.onClick}>
+//             loggin
+//         </button>
+//     )
+// }
+
+// function LogoutButton(props) {
+//     return (
+//         <button onClick={props.onClick}>
+//             Logout
+//         </button>
+//     )
+// }
+
+// class LoginControl extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {isLoggedIn: false};
+//         this.handleLoginClick = this.handleLoginClick.bind(this);
+//         this.handleLogoutClick = this.handleLogoutClick.bind(this);
+//     }
+
+//     handleLoginClick() {
+//         this.setState({isLoggedIn: true});
+//     }
+
+//     handleLogoutClick() {
+//         this.setState({isLoggedIn: false});
+//     }
+
+//     render() {
+//         const isLoggedIn = this.state.isLoggedIn;
+
+//         let button = null;
+//         if (isLoggedIn) {
+//             button = <LogoutButton onClick={this.handleLogoutClick}/>
+//         } else {
+//             button = <LogginButton onClick={this.handleLoginClick} />
+//         }
+
+//         return (
+//             <div>
+//                 <Greeting isLoggedIn={isLoggedIn}/>
+//                 {button}
+//             </div>
+//         )
+//     }
+// }
+
+// ReactDOM.render(
+//     <LoginControl />,
+//     document.getElementById('app')
+// );
+
+
+// class Toggle extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {isToggledOn: true}
+
+//         this.handleClick = this.handleClick.bind(this);
+//     }
+
+//     handleClick() {
+//         this.setState((prevState) => {
+//             return {isToggledOn: !prevState.isToggledOn}
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <button onClick={this.handleClick}>
+//                     {this.state.isToggledOn ? 'ON' : 'OFF'}
+//                 </button>
+//             </div>
+//         )
+//     }
+// }
+
+// ReactDOM.render(
+//     <Toggle />,
+//     document.getElementById('app')
+// )
+
+
+// class Clock extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {date: new Date()};
+//     }
+
+//     componentDidMount() {
+//         this.timerID = setInterval(() => {
+//             this.tick();
+//         }, 1000);
+//     }
+
+//     componentWillUnmount() {
+//         clearInterval(this.timerID);
+//     }
+
+//     tick() {
+//         this.setState({date: new Date()});
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Hello World</h1>
+//                 <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+//             </div>
+//         )
+//     }
+// }
+
+// function App() {
+//     return (
+//         <div>
+//             <Clock />
+//             <Clock />
+//             <Clock />
+//         </div>
+//     )
+// }
+
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById('app')
+// );
 
 /***/ }),
 /* 15 */
