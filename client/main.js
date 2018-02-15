045b9ba1d6d5ace9d4e711dd6c4028df7630e20d
alexgev <alexgev1997@gmail.com>
2018-02-15 18:38:18 +0300

    react redux training

--- client/main.js
+++ client/main.js
@@ -8,7 +8,7 @@ import App from './App';
 import reducer from './reducers';
 
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); 
 
 
 ReactDOM.render(
