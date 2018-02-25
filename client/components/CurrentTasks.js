e4816d35ec5126ff7d2c0238d6d509f1601bc924
alexgev <alexgev1997@gmail.com>
2018-02-25 16:15:13 +0300

    bind complete function

--- client/components/CurrentTasks.js
+++ client/components/CurrentTasks.js
@@ -3,25 +3,23 @@ import { connect } from 'react-redux';
 import { markTaskAsDone } from '../actions/tasks';
 
 const CurrentTasks = (props) => {
-    let saveId = [];
 
-    const handleCompleteTask = (e) => {
-        props.onCompleteTask(saveId[e.target.parentElement.parentElement.rowIndex - 1]);
+    const handleCompleteTask = (task) => {
+        console.log(task);
+        props.onCompleteTask(task._id);
     }
 
     const tableRowsWithCurrentTasks = props.currentTasks.map((task) => {
-        saveId.push(task._id);
         return (
             <tr key={task._id}>
                 <td>{task.title}</td>
                 <td>{task.text}</td>
                 <td>{`${task.created.date}.${task.created.month}.${task.created.year} ${task.created.hours}:${task.created.minutes}:${task.created.seconds}`}</td>
                 <td>{`${task.finished.date}.${task.finished.month}.${task.finished.year} ${task.finished.hours}:${task.finished.minutes}`}</td>
-                <td><input onClick={handleCompleteTask} type="checkbox" className="current-task-table__complete-input"></input></td>
+                <td><input onClick={handleCompleteTask.bind(null, task)} type="checkbox" className="current-task-table__complete-input"></input></td>
             </tr>
         )
     })
-    saveId.reverse();
     return (
         <table className="current-tasks__table">
             <caption>Current Tasks</caption>
