4c0bac40d0ef93282bf4d929e4b8e68cb16bdb25
alexgev <alexgev1997@gmail.com>
2018-03-03 17:32:02 +0300

    change webpack.config, add production congig, production build

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ const path = require('path');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 
+
+const isProd = process.env.NODE_ENV === 'production';
+const build = new webpack.optimize.UglifyJsPlugin();
+const buildConfig = isProd ? build : console.log('dev');
+
+
 const common = {
     entry: path.join(__dirname, "client/main.js"),
     output: {
@@ -20,10 +26,16 @@ const common = {
                 ]
             }
         ]
-    }
+    },
+    plugins: [
+        buildConfig,
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+        })
+    ]
 }
 
 
-module.exports = function(env) {
+module.exports = function (env) {
     return common;
 }
